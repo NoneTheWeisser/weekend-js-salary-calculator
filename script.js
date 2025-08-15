0d056d3bed4eccaa94b8e724a647f1869bfa5230
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2025-08-15 13:38:46 -0500

    I got the red background to show up

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-    // ready to DOM
-    document.addEventListener("DOMContentLoaded", readyNow);
+// ready to DOM
+document.addEventListener("DOMContentLoaded", readyNow);
 
-    let employees = [];
+let employees = [];
 
-    function readyNow() {
-    console.log("DOM is loaded");
-    }
+function readyNow() {
+  console.log("DOM is loaded");
+}
 
-    function addEmployee(event) {
-    event.preventDefault();
-    //   form inputs
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const employeeId = document.getElementById("employee-Id").value;
-    const title = document.getElementById("title").value;
-    const annualSalary = document.getElementById("annual-salary").value;
+function addEmployee(event) {
+  event.preventDefault();
+  //   form inputs
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const employeeId = document.getElementById("employee-Id").value;
+  const title = document.getElementById("title").value;
+  const annualSalary = document.getElementById("annual-salary").value;
 
-    // alert if a field is empty
-    if (!firstName || !lastName || !employeeId || !title || !annualSalary) {
+  // alert if a field is empty
+  if (!firstName || !lastName || !employeeId || !title || !annualSalary) {
     alert("Please fill out all fields before submitting.");
-    return; 
+    return;
   }
 
-    const submittedEmployee = {
-        firstName,
-        lastName,
-        employeeId,
-        title,
-        annualSalary,
-    };
-    employees.push(submittedEmployee);
+  const submittedEmployee = {
+    firstName,
+    lastName,
+    employeeId,
+    title,
+    annualSalary,
+  };
+  employees.push(submittedEmployee);
 
-    render();
+  render();
 
-    //   clear form fields
-    document.getElementById("first-name").value = "";
-    document.getElementById("last-name").value = "";
-    document.getElementById("employee-Id").value = "";
-    document.getElementById("title").value = "";
-    document.getElementById("annual-salary").value = "";
-    }
+  //   clear form fields
+  document.getElementById("first-name").value = "";
+  document.getElementById("last-name").value = "";
+  document.getElementById("employee-Id").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("annual-salary").value = "";
+}
 
-    function render() {
-    const tableBody = document.getElementById("table-body");
-    tableBody.innerHTML = "";
+function render() {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
 
-    for (let i = 0; i < employees.length; i++) {
-        const employee = employees[i];
+  for (let i = 0; i < employees.length; i++) {
+    const employee = employees[i];
     tableBody.innerHTML += `
             <tr>
                 <td>${employee.firstName}</td>
@@ -57,20 +57,43 @@
                 <td><button onClick="deleteEmployee(${i})">Delete</button></td>
             </tr>
             `;
-    }
-    //   updateTotalSalary();
-    }
+  }
+  updateTotalSalary();
+}
 
-    // some sort of render function so we can combine tasks and fire
+// some sort of render function so we can combine tasks and fire
 
-    // employee table - delete button deleteEmployee(event)
-    function deleteEmployee(index) {
-    employees.splice(index, 1);
-    render();
-    }
+// employee table - delete button deleteEmployee()
+function deleteEmployee(index) {
+  employees.splice(index, 1);
+  render();
+}
 
-    // updating the total monthly salary
-    // updateTotalSalary()
+// updating the total monthly salary
+function updateTotalSalary() {
+  let total = 0;
+  for (let employee of employees) {
+    const monthlySalary = Number(employee.annualSalary) / 12;
+    total += monthlySalary;
+  }
 
-    // things to keep in mind, if we are doing stretch goal,
-    //  we will need to be able to subtract their annual salary's from the total
+  const totalMonthlyElement = document.getElementById("monthly-total");
+  totalMonthlyElement.textContent = `Total Monthly: $${total.toLocaleString()}`;
+
+  highMonthlySalary(total);
+}
+
+function highMonthlySalary(total) {
+  const monthlyTotalElement = document.getElementById("monthly-total");
+  // Looking for high salary
+  if (total > 20000) {
+    monthlyTotalElement.classList.add("high-monthly-total");
+  } else {
+    monthlyTotalElement.classList.remove("high-monthly-total");
+  }
+}
+
+// A 'Submit' button should collect the form information, store the information to calculate monthly costs,
+// append information to the DOM and clear the input fields. Using the stored information,
+// calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000,
+//  add a red background color to the total monthly cost.
